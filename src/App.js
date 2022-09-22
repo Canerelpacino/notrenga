2cb906d94227df7ac013d854c49347d1669bdbfd
Canerelpacino <canersmedia@gmail.com>
2022-09-22 19:39:19 +0200

    m

--- src/App.js
+++ src/App.js
@@ -79,8 +79,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 5) {
-      newMintAmount = 5;
+    if (newMintAmount > 1) {
+      newMintAmount = 1;
     }
     setMintAmount(newMintAmount);
   };
